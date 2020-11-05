5da19485c249dbdcab157f35d0149fbb5d65ba34
Marina Domashenko <creator.marina@gmail.com>
2020-11-05 23:03:15 +0200

    Add task color swich

--- src/js/color-switch.js
+++ src/js/color-switch.js
@@ -7,6 +7,34 @@ const colors = [
   '#795548',
 ];
 
+const refs = {
+  body: document.querySelector('body'),
+  buttonStart: document.querySelector('[data-action="start"]'),
+  buttonStop: document.querySelector('[data-action="stop"]'),
+};
+
+const DELAY = 1000;
+let timerId = null;
+let isActive = false;
+
+refs.buttonStart.addEventListener('click', onChangeColor);
+refs.buttonStop.addEventListener('click', onStopChangeColor);
+
+function onChangeColor() {
+  if (isActive === false) {
+    isActive = true;
+    timerId = setInterval(() => {
+      refs.body.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, DELAY);
+  }
+}
+
+function onStopChangeColor() {
+  clearTimeout(timerId);
+  isActive = false;
+}
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
